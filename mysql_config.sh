19ae09892fba267674c800c6ea888a52a437a63e
miler2 <40854926+miler2@users.noreply.github.com>
2023-10-24 14:25:05 +0200

    Test

--- mysql_config.sh
+++ mysql_config.sh
@@ -4,3 +4,7 @@
 docker exec -it mysql bash
 
 mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Miler';"
+mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Miler';"
+mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Miler';"
+mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Miler';"
+mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Miler';"
