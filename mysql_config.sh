235d583d8054d8f6755c5ea51b0b6f0b5198e49c
miler2 <40854926+miler2@users.noreply.github.com>
2023-10-24 14:11:33 +0200

    Quitamos linea con variable

--- mysql_config.sh
+++ mysql_config.sh
@@ -3,5 +3,4 @@
 #Entramos a la consola de nuestro docker MySQL
 docker exec -it mysql bash
 
-#mysql -u root -p$password <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Miler';"
 mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Miler';"
