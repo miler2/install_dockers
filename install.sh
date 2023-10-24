4fd8f52da6aa922d8d0e99c52800517bb179bed3
miler2 <40854926+miler2@users.noreply.github.com>
2023-10-24 14:10:05 +0200

    Terminamos archivo install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 #Variables
 #password="Miler"
 
-apt update
-apt upgrade
+apt update -y
+apt upgrade -y
 
 #Instalamos docker en nuestra máquina
 apt install apt-transport-https ca-certificates curl software-properties-common -y
@@ -13,9 +13,3 @@ apt install docker-ce -y
 
 #Creamos el docker MySQL con persistencia de datos:
 docker run -d -p 33060:3306 --name mysql -v /home/ubuntu/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Miler mysql
-
-#Entramos a la consola de nuestro docker MySQL
-docker exec -it mysql bash
-
-#mysql -u root -p$password <<< "ALTER USER 'root'@'localhost' IDENTIFIED BY 'Miler';"
-mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Miler';"
