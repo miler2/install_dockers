26d8a7ce6cd97357de624762650515b1b5056173
miler2 <vegavegewtu@gmail.com>
2023-10-30 08:32:13 +0000

    He anadido archivo nuevo

--- mysql_config.sh
+++ mysql_config.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 #Entramos a la consola de nuestro docker MySQL
-docker exec -it mysql bash
-
-mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Miler';"
+docker exec -it mysql bash <<< mysql -u root -pMiler <<< "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Miler';"
---
+++ phpmyadmin.sh
@@ -0,0 +1,1 @@
+docker run -d --rm --name phpmyadminc --link mysql -e PMA_HOST=mysql -p 8080:80 phpmyadmin
