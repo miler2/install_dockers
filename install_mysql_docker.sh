d25f88655feb6e7e75c13e711c81c6c88746ae0f
miler2 <vegavegewtu@gmail.com>
2023-10-30 08:56:12 +0000

    He renombrado archivo install.sh

---
+++ install_mysql_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update -y
+apt upgrade -y
+
+#Instalamos docker en nuestra máquina
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+apt install docker-ce -y
+
+#Creamos el docker MySQL con persistencia de datos:
+#Usamos el archivo mysql.sh
+./mysql.sh
+
+#Hacemos la configuración del docker mysql
+./mysql_config.sh
