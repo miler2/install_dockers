9756133b419ed605414093ae3f13dca521501203
miler2 <40854926+miler2@users.noreply.github.com>
2023-10-30 13:08:27 +0100

    Delete tarea_scripts directory

--- tarea_scripts/mysql.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Este comando es la copia del archivo mysql.sh en scripts pero con "--volumen" añadido.
-docker run -d --rm -p 33060:3306 --name mysql --volume=/root/docker/mysql/conf.d:/etc/mysql/conf.d -v /home/ubuntu/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Miler mysql
--- tarea_scripts/show_global_variables.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Este comando actualmente no funciona. Es una prueba.
-mysql -u root -pMiler -e 'show global variables like "max_connections"';
