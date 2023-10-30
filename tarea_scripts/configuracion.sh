892e11f02df652b22403fa24350b17c089eb56c8
miler2 <vegavegewtu@gmail.com>
2023-10-30 09:22:19 +0000

    He añadido cosas

--- tarea_scripts/configuracion.sh
+++ tarea_scripts/configuracion.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker pull mysql/mysql-server:latest
+mkdir -p /root/docker/mysql/conf.d
+
+#Esto es lo que tendríamos que hacer si lo estuviéramos haciendo manualmente
+#Nosotros ya tenemos el archivo creado, a si que simplemente lo copiamos y lo movemos al directiorio.
+#nano /root/docker/mysql/conf.d/my-custom.cnf
+
+> /root/docker/mysql/conf.d/my-custom.cnf
+cp my-custom.cnf /root/docker/mysql/conf.d/my-custom.cnf
