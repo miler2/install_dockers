d0b0a839459a9f924a8f6a585f7b2a8eedb78aef
miler2 <vegavegewtu@gmail.com>
2023-10-30 09:55:57 +0000

    Si

--- tarea_scripts/configuracion.sh
+++ tarea_scripts/configuracion.sh
@@ -5,5 +5,12 @@ mkdir -p /root/docker/mysql/conf.d
 #Nosotros ya tenemos el archivo creado, a si que simplemente lo copiamos y lo movemos al directiorio.
 #nano /root/docker/mysql/conf.d/my-custom.cnf
 
+#Creamos el archivo my-custom.cnf dentro del docker
 > /root/docker/mysql/conf.d/my-custom.cnf
+
+#Copiamos nuestro archivo local al docker para hacerle la configuración.
 cp my-custom.cnf /root/docker/mysql/conf.d/my-custom.cnf
+
+#Eliminamos el docker y lo ejecutamos de nuevo.
+docker rm mysql
+./../scripts/mysql.sh
