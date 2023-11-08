66320acd24670da138a5ec2707b1c25c08dc9697
miler2 <vegavegewtu@gmail.com>
2023-11-08 10:56:17 +0000

    He corregido algunos errores en configuración de mysql

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source scripts/.env
-docker run -d --rm -p 33060:3306 --name mysql -v /home/ubuntu/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$password_docker_mysql mysql
+docker run -d --rm -p 3306:3306 --name mysql -v /home/ubuntu/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$password_docker_mysql mysql
--- scripts/mysql_config.sh
+++ scripts/mysql_config.sh
@@ -2,7 +2,8 @@
 
 source scripts/.env
 
-docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '$password_docker_mysql'\""
+docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_docker_mysql'\""
+docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$password_docker_mysql'\""
 
 #old_password=hola
 #new_password=hola
