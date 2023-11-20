d581c9273734aed621ef8899a58b18017120a6b6
miler2 <vegavegewtu@gmail.com>
2023-11-20 11:56:33 +0000

    He cambiado el nombre de la variable para usuario_root

--- scripts/mysql_config.sh
+++ scripts/mysql_config.sh
@@ -2,9 +2,5 @@
 
 source scripts/.env
 
-docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_docker_mysql'\""
-docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$password_docker_mysql'\""
-
-#old_password=hola
-#new_password=hola
-#sudo docker exec -i mysql bash -c "mysql -u root -p$old_password <<< \"ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$new_password'\""
+docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER root@localhost IDENTIFIED WITH caching_sha2_password BY '$password_user_root'\""
+docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER 'root'@'%' IDENTIFIED WITH caching_sha2_password BY '$password_user_root'\""
