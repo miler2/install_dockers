e85aad1563a77cc74e88fac2abd9b6ad63ede6fd
miler2 <40854926+miler2@users.noreply.github.com>
2024-01-08 09:55:12 +0100

    Update mysql.sh

--- scripts/mysql.sh
+++ scripts/mysql.sh
@@ -3,4 +3,4 @@
 source scripts/.env
 
 docker stop mysql
-docker run -d --rm -p 3306:3306 --name mysql -v /home/ubuntu/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$password_docker_mysql mysql
+docker run -d --rm -p 3306:3306 --name mysql -v /home/ubuntu/data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$password_user_root mysql
