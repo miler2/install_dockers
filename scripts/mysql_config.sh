53e9cfc356a78bd9fe202571368bc98271eb353f
miler2 <40854926+miler2@users.noreply.github.com>
2024-01-08 09:55:35 +0100

    Update mysql_config.sh

--- scripts/mysql_config.sh
+++ scripts/mysql_config.sh
@@ -2,5 +2,5 @@
 
 source scripts/.env
 
-docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'\""
-docker exec -i mysql bash -c "mysql -u root -p$password_docker_mysql <<< \"ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'\""
+docker exec -i mysql bash -c "mysql -u root -p$password_user_root <<< \"ALTER USER root@localhost IDENTIFIED WITH mysql_native_password BY '$password_user_root'\""
+docker exec -i mysql bash -c "mysql -u root -p$password_user_root <<< \"ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '$password_user_root'\""
